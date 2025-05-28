0579c335fe63a42b90092b60e1b80cbf55eb3e66
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-05-28 23:46:12 +0300

    hihihi
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- scripts/3_projects.sh
+++ scripts/3_projects.sh
@@ -4,13 +4,11 @@ mkdir -p /projects/personal
 mkdir -p /projects/work
 
 # common
-read -s -p "Vault password: " vault_pass
-echo
-
 git clone https://github.com/iypetrov/vault.git /projects/common/vault
-find /projects/common/vault/.ssh -type f -exec ansible-vault decrypt --vault-password-file=<(echo "${vault_pass}") {} \;
-find /projects/common/vault/auth_codes -type f -exec ansible-vault decrypt --vault-password-file=<(echo "${vault_pass}") {} \;
+find /projects/common/vault/.ssh -type f -exec ansible-vault decrypt --ask-vault-pass {} \;
+find /projects/common/vault/auth_codes -type f -exec ansible-vault decrypt --ask-vault-pass {} \;
 
+rm -rf /home/ipetrov/.ssh
 ln -sfn /projects/common/vault/.ssh /home/ipetrov
 ln -sfn /projects/common/vault/auth_codes /home/ipetrov
 
@@ -18,5 +16,3 @@ git clone --branch universal-config-v2 git@github.com:iypetrov/.dotfiles.git /pr
 stow -t /home/ipetrov .
 
 git clone git@github.com:iypetrov/books.git /projects/common/books
-
-rm /tmp/vault_pass
