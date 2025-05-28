8b19d1c0f953d4596016d353b07a47f1f12830ed
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-05-28 23:13:34 +0300

    ufudshfusd
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- scripts/3_projects.sh
+++ scripts/3_projects.sh
@@ -4,13 +4,18 @@ mkdir -p /projects/personal
 mkdir -p /projects/work
 
 # common
+read -s -p "Vault password: " vault_pass
+echo "${vault_pass}" > /tmp/vault_pass
+
 git clone https://github.com/iypetrov/vault.git /projects/common/vault
-find /projects/common/vault/.ssh -type f -exec ansible-vault decrypt --ask-vault-pass {} \;
-find /projects/common/vault/auth_codes -type f -exec ansible-vault decrypt --ask-vault-pass {} \;
-ln -sfn /projects/common/vault/.ssh /home/ipetrov/.ssh
-ln -sfn /projects/common/vault/auth_codes /home/ipetrov/auth_codes
+find /projects/common/vault/.ssh -type f -exec ansible-vault decrypt --vault-password-file /tmp/vault_pass {} \;
+find /projects/common/vault/auth_codes -type f -exec ansible-vault decrypt --vault-password-file /tmp/vault_pass {} \;
+ln -sfn /projects/common/vault/.ssh /home/ipetrov
+ln -sfn /projects/common/vault/auth_codes /home/ipetrov
 
 git clone --branch universal-config-v2 git@github.com:iypetrov/.dotfiles.git /projects/common/.dotfiles
 stow -t /home/ipetrov .
 
 git clone git@github.com:iypetrov/books.git /projects/common/books
+
+rm /tmp/vault_pass
