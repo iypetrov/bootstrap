12e4d3ebea9093cce52b8ba56f880bf0d0b2b6d9
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-05-28 17:43:57 +0300

    sym links
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- scripts/3_projects.sh
+++ scripts/3_projects.sh
@@ -7,6 +7,8 @@ mkdir -p /projects/work
 git clone https://github.com/iypetrov/vault.git /projects/common/vault
 ansible-vault decrypt /projects/common/vault/.ssh/*
 ansible-vault decrypt /projects/common/vault/auth_codes/*
+ln -sfn /projects/common/vault/.ssh /home/ipetrov/.ssh
+ln -sfn /projects/common/vault/auth_codes /home/ipetrov/auth_codes
 
 git clone --branch universal-config-v2 git@github.com:iypetrov/.dotfiles.git /projects/common/.dotfiles
 stow -t /home/ipetrov .
