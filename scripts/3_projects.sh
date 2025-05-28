78e40557df61a649d78f71d6683c7203c65b9b7a
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-05-28 17:03:09 +0300

    dd
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- scripts/3_projects.sh
+++ scripts/3_projects.sh
@@ -5,8 +5,9 @@ mkdir -p /projects/work
 
 # common
 git clone https://github.com/iypetrov/vault.git /projects/common/vault
-ansible-vault decrypt /projects/common/vault/*   
-stow -t /home/$(whoami) .
+ansible-vault decrypt /projects/common/vault/.ssh/*
+ansible-vault decrypt /projects/common/vault/auth_codes/*
+stow -t /home/ipetrov .
 
 git clone git@github.com:iypetrov/.dotfiles.git /projects/common/.dotfiles
 git clone git@github.com:iypetrov/books.git /projects/common/books
