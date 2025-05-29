f29df60f5fdc60e189ad7ac0bed3f91346c5b473
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-05-29 15:15:12 +0300

    use ansipble pass
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- scripts/3_projects.sh
+++ scripts/3_projects.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir -p /projects/common
 mkdir -p /projects/personal
 mkdir -p /projects/work
 
@@ -7,11 +8,10 @@ mkdir -p /projects/work
 git clone https://github.com/iypetrov/vault.git /projects/common/vault
 ln -sfn /projects/common/vault /projects/common
 
-read -p "Enter your Ansible password: " pass
-echo "${pass}" > pass.txt
+ansible-vault decrypt --ask-vault-pass /projects/common/vault/ansible-vault-pass.txt
 
-find /projects/common/vault/.ssh -type f -exec ansible-vault decrypt --vault-password-file pass.txt {} \;
-find /projects/common/vault/auth_codes -type f -exec ansible-vault decrypt --vault-password-file pass.txt {} \;
+find /projects/common/vault/.ssh -type f -exec ansible-vault decrypt --vault-password-file /projects/common/vault/ansible-vault-pass.txt {} \;
+find /projects/common/vault/auth_codes -type f -exec ansible-vault decrypt --vault-password-file /projects/common/vault/ansible-vault-pass.txt {} \;
 
 rm -rf /home/ipetrov/.ssh
 ln -sfn /projects/common/vault/.ssh /home/ipetrov
@@ -25,5 +25,3 @@ git -C /projects/common/.dotfiles remote set-url origin git@github.com:iypetrov/
 
 git clone git@github.com:iypetrov/books.git /projects/common/books
 ln -sfn /projects/common/books /projects/common
-
-rm pass.txt
