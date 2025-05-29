c8b5a5cc76f9080189f9cc2311abc6e8c15417df
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-05-29 22:03:54 +0300

    add a function for better deps installations
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- scripts/1_tools.sh
+++ scripts/1_tools.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-apt install -y \
-    open-vm-tools \
-    open-vm-tools-desktop \
-    curl \
-    wget \
-    tmux \
-    make \
-    vim \
-    stow \
-    software-properties-common \
-    zsh
+function install_dep() {
+    local cmd=$1
+    
+    if which "${cmd}" > /dev/null 2>&1; then
+        echo "🔕 Skipped isntalling ${cmd} dependency, already available"
+    else
+        echo "🔧 Installing ${cmd} dependency"
+        if apt install -y "${cmd}" > "/var/log/deps/${cmd}.log" 2>&1; then
+            echo "✅ ${cmd} dependency installed successfully"
+        else
+            echo "❌ ${cmd} dependency failed to installed"
+        fi
+    fi
+}
+
+install_dep open-vm-tools
+install_dep open-vm-tools-desktop
+install_dep curl
+install_dep wget
+install_dep tmux
+install_dep make
+install_dep vim
+install_dep stow
+install_dep software-properties-common
+install_dep zsh
 
 apt-add-repository --yes --update ppa:ansible/ansible
 
-apt install -y \
-    ansible
+install_dep ansible
