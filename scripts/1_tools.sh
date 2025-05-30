10f47d1cb84108a9f752ca51d5c6a6f679b283cf
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-05-30 13:42:08 +0300

    a bit different
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- scripts/1_tools.sh
+++ scripts/1_tools.sh
@@ -22,8 +22,6 @@ function install_dep() {
 mkdir -p /var/log/deps
 
 install_dep zsh
-install_dep open-vm-tools
-install_dep open-vm-tools-desktop
 install_dep curl
 install_dep tmux
 install_dep make
