e63cfa3dc14ff172690393193adf490e3915dc85
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-06-13 12:51:21 +0300

    big, lets try
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- main.sh
+++ main.sh
@@ -14,6 +14,7 @@ fi
 
 # Dependencies
 apt update
+wget -P /tmp/delta https://github.com/dandavison/delta/releases/download/0.18.1/git-delta_0.18.1_arm64.deb
 apt install -y \
   curl \
   unzip \
@@ -21,7 +22,7 @@ apt install -y \
   stow \
   ansible \
   sudo \
-  vim-gtk3 \
+  vim \
   tmux \
   fzf \
   build-essential \
@@ -32,7 +33,26 @@ apt install -y \
   software-properties-common \
   lazygit \
   docker.io \
-  silversearcher-ag
+  silversearcher-ag \
+  /tmp/delta \
+  bat
+
+# tmux
+touch "/home/$USERNAME/.tmux/last_session"
+
+# aws
+wget -P /tmp/awscliv2 https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip
+mv /tmp/awscliv2/*.zip /tmp/awscliv2/awscliv2.zip
+unzip /tmp/awscliv2/awscliv2.zip -d /tmp/awscliv2
+/tmp/awscliv2/aws/install
+
+# docker
+usermod -aG docker "$USERNAME"
+newgrp docker
+
+# kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl.sha256"
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 # User 
 if ! id "$USERNAME" &>/dev/null; then
@@ -43,6 +63,7 @@ echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/$USERNAME"
 chmod 0440 "/etc/sudoers.d/$USERNAME"
 
 # Setup projects
+mv /projects/common/bootstrap /home/$USERNAME/projects/common/bootstrap
 sudo -u "$USERNAME" bash << EOF
 # setup
 rm -rf /home/$USERNAME/.ssh
@@ -59,8 +80,8 @@ git clone https://${GH_USERNAME}:${GH_PAT}@github.com/iypetrov/.dotfiles.git /ho
 cd /home/$USERNAME/projects/common
 stow --target=/home/$USERNAME/.dotfiles
 git clone https://github.com/tmux-plugins/tpm /home/$USERNAME/.tmux/plugins/tpm
-find /home/$USERNAME/projects/common/vault/.ssh -type f -exec ansible-vault encrypt --vault-password-file /tmp/ansible-vault-pass.txt {} \;
-find /home/$USERNAME/projects/common/vault/.aws -type f -exec ansible-vault encrypt --vault-password-file /tmp/ansible-vault-pass.txt {} \;
+cd /home/$USERNAME/projects/common/bootstrap
+git remote set-url origin git@github.com:iypetrov/bootstrap.git
 cd /home/$USERNAME/projects/common/vault
 git remote set-url origin git@github.com:iypetrov/vault.git
 cd /home/$USERNAME/projects/common/.dotfiles
@@ -68,18 +89,40 @@ git remote set-url origin git@github.com:iypetrov/.dotfiles.git
 rm /tmp/ansible-vault-pass.txt
 
 # common
-# personal
-# ip812
-# avalon
-# work
-EOF
+cd /home/$USERNAME/projects/common
+git clone git@github.com:iypetrov/books.git
 
-# tmux
-touch "/home/$USERNAME/.tmux/last_session"
+# personal
+cd /home/$USERNAME/projects/personal
+git clone git@github.com:iypetrov/go-playground.git
+git clone git@github.com:iypetrov/aws-playground.git
+git clone git@github.com:iypetrov/k8s-playground.git
+git clone git@github.com:iypetrov/lambdas.git
+
+# ip812
+cd /home/$USERNAME/projects/ip812
+git clone git@github.com:ip812/infra.git
+git clone git@github.com:ip812/go-template.git
+git clone git@github.com:ip812/lambdas.git
+
+# avalon
+cd /home/$USERNAME/projects/avalon
+git clone git@github.com:avalonpharma/infra.git
+git clone git@github.com:avalonpharma/avalon-ui.git
+git clone git@github.com:avalonpharma/avalon-rest.git
+
+# work
+CPX_USERNAME="ilia.petrov"
+CPX_PAT="$(cat /home/$USERNAME/projects/common/vault/auth_codes/cpx-gitlab.txt)"
+cd /home/$USERNAME/projects/work
+git clone https://$CPX_USERNAME:$CPX_PAT@innersource.soprasteria.com/ENER-GXrestricted/infrastructure/terraform/tf-de-gasx.git
+git clone https://$CPX_USERNAME:$CPX_PAT@innersource.soprasteria.com/ENER-GXrestricted/infrastructure/terraform/tf-de-lab52.git
+git clone https://$CPX_USERNAME:$CPX_PAT@innersource.soprasteria.com/ENER-GXrestricted/infrastructure/terraform/tf-de-lab12.git
+git clone https://$CPX_USERNAME:$CPX_PAT@innersource.soprasteria.com/ENER-GXrestricted/infrastructure/terraform/tf-de-lab09.git
+git clone https://$CPX_USERNAME:$CPX_PAT@innersource.soprasteria.com/ENER-GXrestricted/infrastructure/terraform/tf-ci-library.git
+git clone https://$CPX_USERNAME:$CPX_PAT@innersource.soprasteria.com/ENER-GXrestricted/infrastructure/salt/salt.git
+git clone https://$CPX_USERNAME:$CPX_PAT@innersource.soprasteria.com/ENER-GXrestricted/infrastructure/salt/pillar.git
+EOF
 
 # zsh
 chsh -s "$(which zsh)" "$USERNAME"
-
-# docker
-usermod -aG docker "$USERNAME"
-newgrp docker
