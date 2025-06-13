28545e1bb3b95dc5ee6f1d7d10ee8a6fc622f5a1
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-06-14 00:43:10 +0300

    raw
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- main.sh
+++ main.sh
@@ -2,9 +2,9 @@
 
 USERNAME="ipetrov"
 read -rp "GitHub Username: " GH_USERNAME
-read -srp "GitHub Personal Access Token: " GH_PAT
+read -sr "GitHub Personal Access Token: " GH_PAT
 echo
-read -srp "Ansible Vault Password: " ANSIBLE_VAULT_PASSWORD
+read -sr "Ansible Vault Password: " ANSIBLE_VAULT_PASSWORD
 echo
 
 if [[ $EUID -ne 0 ]]; then
